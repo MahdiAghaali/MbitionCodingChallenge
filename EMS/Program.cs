8e0bf600797a42ad3c116a950e7530a4c0e1165b
Mahdi aghaali <mahdi.aghaali@yahoo.com>
2025-03-10 09:18:57 +0100

    EMS-05 | Added method selection screen

--- EMS/Program.cs
+++ EMS/Program.cs
@@ -6,7 +6,26 @@ namespace EventManagementSystem
         static void Main()
         {
             EventManager manager = new EventManager();
-            manager.CreateEvent();
+            while (true)
+            {
+                Console.WriteLine("Event Management System");
+                Console.WriteLine("1. Add Event");
+                Console.WriteLine("2. View Events");
+                Console.WriteLine("0. Exit");
+                Console.Write("Choose an option: ");
+
+                switch (Console.ReadLine())
+                {
+                    case "1": manager.CreateEvent(); break;
+                    // case "2": manager.ViewEvents(); break;
+                    case "0": return;
+                    default: 
+                    {   
+                        Console.Clear();
+                        Console.WriteLine("Invalid Input! Please try again. \n"); 
+                    } break;
+                }
+            }
         }
     }
 }
